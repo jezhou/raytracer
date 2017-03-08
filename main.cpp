88d5b3e4f696c4cb96fdd4582aece12ef4b24e06
Jesse Zhou <jesse.cy.zhou@gmail.com>
2017-03-07 17:01:20 -0800

    figured out how to use freeimage

--- main.cpp
+++ main.cpp
@@ -8,13 +8,31 @@
 #include <FreeImage.h>
 using namespace std;
 
-int main(int argc, char* argv[]) {
-  if (argc < 2) {
-    cerr << "Usage: raytracer <scenefile>\n";
-    exit(-1);
-  }
-  
-  FreeImage_Initialise();
+#define WIDTH 800
+#define HEIGHT 600
+#define BITS_PER_PIXEL 24 // 3 8-bit rgb values = 24
 
+int main(int argc, char* argv[]) {
+
+  // TODO: Uncomment this out later
+  // if (argc < 2) {
+  //   cerr << "Usage: raytracer <scenefile>\n";
+  //   exit(-1);
+  // }
+
+  FreeImage_Initialise();
+  FIBITMAP * bitmap = FreeImage_Allocate(WIDTH, HEIGHT, BITS_PER_PIXEL);
+  RGBQUAD color;
+
+  for(int i = 0; i < WIDTH; i++) {
+    for(int j = 0; j< HEIGHT; j++) {
+      color.rgbRed = 0;
+      color.rgbGreen = (double)i / WIDTH * 255;
+      color.rgbBlue = (double)j / HEIGHT;
+      FreeImage_SetPixelColor(bitmap, i, j, &color);
+    }
+  }
+
+  FreeImage_Save(FIF_PNG, bitmap, "test.png", 0);
   FreeImage_DeInitialise();
 }
